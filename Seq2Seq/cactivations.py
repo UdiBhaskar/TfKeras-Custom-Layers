4d30ad5587c7cd21457f453db70dedaa45198ae5
Uday Bhaskar <udaybhaskarpaila@gmail.com>
2019-08-30 22:26:32 +0530

    adding BahdanauAttention

--- Seq2Seq/cactivations.py
+++ Seq2Seq/cactivations.py
@@ -1,8 +1,9 @@
-'''activations'''
+'''Custom activations'''
 import warnings
 import six
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
+from tensorflow.keras.utils import deserialize_keras_object
 
 def softmax(inputs, axis=-1):
     '''softmax activation function
@@ -26,5 +27,70 @@ def hardmax(inputs, axis=-1):
     out = tf.one_hot(tf.math.argmax(inputs, axis), out_shape)
     return out
 
-def sparsemax(parameter_list):
-    pass
+def sparsemax(inputs):
+    '''sparsemax
+    Implementation is based on below paper
+    https://arxiv.org/pdf/1602.02068.pdf
+    # Arguments
+        inputs: Input Tensor
+    # Returns
+        Tensor, output of the sparsemax transformation
+    '''
+
+    inputs = tf.convert_to_tensor(inputs, name='logits')
+    shape_inputs = tf.shape(inputs)
+    input_sorted, _ = tf.math.top_k(inputs, k=shape_inputs[1])
+    input_cumsum = tf.cumsum(input_sorted, axis=-1)
+    k = tf.range(1, tf.cast(shape_inputs[1], inputs.dtype)+1, dtype=inputs.dtype)
+    input_check = 1 + k*input_sorted > input_cumsum
+    k_input = tf.reduce_sum(tf.cast(input_check, tf.int32), axis=1)
+
+    #calculating tau(z)
+    k_input_safe = tf.maximum(k_input, 1)
+    indices = tf.stack([tf.range(0, shape_inputs[0]), k_input_safe-1], axis=1)
+    tau_sum = tf.gather_nd(input_cumsum, indices)
+    tau_input = (tau_sum - 1) / tf.cast(k_input, inputs.dtype)
+
+    prob_sparse = tf.maximum(tf.cast(0, inputs.dtype), inputs - tau_input[:, tf.newaxis])
+
+    prob_sparse = tf.where(tf.logical_or(tf.equal(k_input, 0), \
+        tf.math.is_nan(input_cumsum[:, -1])), tf.fill([shape_inputs[0], shape_inputs[1]], \
+            tf.cast(float("nan"), inputs.dtype)), prob_sparse)
+    return prob_sparse
+
+def serialize(activation):
+    '''Get the name of activation'''
+    return activation.__name__
+
+def deserialize(name, custom_objects=None):
+    '''deserialize Keras Object'''
+    return deserialize_keras_object(
+        name,
+        module_objects=globals(),
+        custom_objects=custom_objects,
+        printable_module_name='activation function')
+
+def get(identifier):
+    """Get the `identifier` activation function.
+    # Arguments
+        identifier: None or str, name of the function.
+    # Returns
+        The activation function, `linear` if `identifier` is None.
+    # Raises
+        ValueError if unknown identifier
+    """
+    if isinstance(identifier, six.string_types):
+        identifier = str(identifier)
+        return deserialize(identifier)
+    elif callable(identifier):
+        if isinstance(identifier, Layer):
+            warnings.warn(
+                'Do not pass a layer instance (such as {identifier}) as the '
+                'activation argument of another layer. Instead, advanced '
+                'activation layers should be used just like any other '
+                'layer in a model.'.format(
+                    identifier=identifier.__class__.__name__))
+        return identifier
+    else:
+        raise ValueError('Could not interpret '
+                         'activation function identifier:', identifier)
--- Seq2Seq/clayers.py
+++ Seq2Seq/clayers.py
@@ -2,26 +2,74 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Layer
+from .cactivations import get as cget
 
 
 class BahdanauAttention(Layer):
-    '''BahdanauAttention'''
-    def __init__(self, units, probability_fn=None, **kwargs):
+    '''
+    BahdanauAttention:
+    Implemented based on below paper
+        https://arxiv.org/pdf/1409.0473.pdf
+        attention_weights = probability_fn(Va * tanh(Wa*Ht+Ua*Hs+b))
+    # Arguments
+        units = number of hidden units to use.
+        probability_fn = probability function to get probabilities(weights for attention)
+                         You can use 'softmax' or 'hardmax' or 'sparsemax' or any custom
+                         function which takes input distribution and returns probability dist.
+        dropout_rate = dropout for attention weights (between 0 and 1, 0 - no dropout).
+        return_aweights = Bool, whether to return attention weights or not.
+        weights_initializer = initializer for weight matrix
+        bias_initializer = initializer for bias values
+        weights_constraint = Constraint function applied to the weights
+        bias_constraint = Constraint function applied to the bias
+    # Returns
+        context_vector = context vector after applying attention.
+        attention_weights = attention weights only if `return_aweights=True`.
+
+    # Inputs to the layer
+        inputs = dictionary with keys "enocderHs", "decoderHt".
+                enocderHs = all the encoder hidden states,
+                            shape - (Batchsize, encoder_seq_len, enc_hidden_size)
+                 decoderHt = hidden state of decoder at that timestep,
+                            shape - (Batchsize, dec_hidden_size)
+        mask = You can apply mask for padded values or any custom values
+               while calculating attention.
+               mask shape should be (Batchsize, encoder_seq_len)
+    '''
+    def __init__(self, units,
+                 probability_fn='softmax',
+                 dropout_rate=0,
+                 return_aweights=True,
+                 weights_initializer='he_normal',
+                 bias_initializer='zeros',
+                 weights_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
 
         if 'name' not in kwargs:
             kwargs['name'] = ""
         super(BahdanauAttention, self).__init__(**kwargs)
 
         self.units = units
-        self._wa = layers.Dense(self.units, name=self.name+"Wa", use_bias=False)
-        self._ua = layers.Dense(self.units, name=self.name+"Ua")
-        self._va = layers.Dense(1, name=self.name+"Va")
         self.probability_fn = probability_fn
-
-        if probability_fn is None:
-            self.probability_fn = tf.nn.softmax
-        else:
-            self.probability_fn = probability_fn
+        self.dropout_rate = dropout_rate
+        self.return_aweights = return_aweights
+        self.probability_fn = cget(probability_fn)
+        self.weights_initializer = weights_initializer
+        self.bias_initializer = bias_initializer
+        self.weights_constraint = weights_constraint
+        self.bias_constraint = bias_constraint
+        self._wa = layers.Dense(self.units, use_bias=False,\
+            kernel_initializer=self.weights_initializer, bias_initializer=self.bias_initializer,\
+                kernel_constraint=self.weights_constraint, bias_constraint=self.bias_constraint,\
+                    name=self.name+"Wa")
+        self._ua = layers.Dense(self.units,\
+            kernel_initializer=self.weights_initializer, bias_initializer=self.bias_initializer,\
+                kernel_constraint=self.weights_constraint, bias_constraint=self.bias_constraint,\
+                    name=self.name+"Ua")
+        self._va = layers.Dense(1, kernel_initializer=self.weights_initializer,\
+            bias_initializer=self.bias_initializer, kernel_constraint=self.weights_constraint,\
+                bias_constraint=self.bias_constraint, name=self.name+"Va")
 
 
     def build(self, input_shape):
@@ -39,50 +87,49 @@ class BahdanauAttention(Layer):
         self.built = True # pylint: disable=W0201
 
     def call(self, inputs, mask=None):
-        '''call
-        enc_out, dec_prev_hs = inputs['enocderHs'], inputs['decoderHt']
-        inputs - dict {'enocderHs':'hs', 'decoderht':'ht'}'''
-
-        assert len(inputs) == 2, "inputs length must be 2 but got {}".format(len(inputs))
+        '''call'''
+        assert isinstance(inputs, dict)
 
         if ('enocderHs' not in inputs.keys())or ('decoderHt' not in inputs.keys()):
             raise ValueError("Input to the layer must be a dict with \
             keys=['enocderHs','decoderHt']")
 
-        enc_out, dec_prev_hs = inputs['enocderHs'], inputs['decoderHt']
+        enc_out, dec_prev_hs = tf.cast(inputs['enocderHs'], tf.float64), \
+            tf.cast(inputs['decoderHt'], tf.float64)
+
         # decprev_hs - Decoder hidden shape == (batch_size, hidden size)
         # hidden_with_time_axis shape == (batch_size, 1, hidden size)
-        hidden_with_time_axis = tf.expand_dims(dec_prev_hs, 1)
+        dec_hidden_with_time_axis = tf.expand_dims(dec_prev_hs, 1)
 
         # score shape == (batch_size, max_length)
-        # we get 1 at the last axis because we are applying score to self.V
-        # the shape of the tensor before applying self.V is (batch_size, max_length, units)
-        score = self._va(tf.nn.tanh(self._wa(hidden_with_time_axis) + self._ua(enc_out)))
+        score = self._va(tf.nn.tanh(self._wa(dec_hidden_with_time_axis) + self._ua(enc_out)))
         score = tf.squeeze(score, [2])
 
         if mask is not None:
-            enc_out = enc_out +(mask* -1e9)
+            score = score + (mask*-1e9)
 
         # attention_weights shape == (batch_size, max_length)
         attention_weights = self.probability_fn(score, axis=-1)
         #(batch_size, max_length, 1)
         attention_weights = tf.expand_dims(attention_weights, 2)
-        # context_vector shape (batch_size, 1, hidden_size)
+
+        if self.dropout_rate != 0:
+            attention_weights = tf.nn.dropout(attention_weights, rate=self.dropout_rate)
+
+        #context_vector shape (batch_size, hidden_size)
         context_vector = tf.tensordot(attention_weights, enc_out, axes=2)
 
-        context_vector = tf.expand_dims(context_vector, 1)
-
+        if self.return_aweights:
+            return context_vector, tf.squeeze(attention_weights, 2)
         return context_vector
 
     def compute_output_shape(self, input_shape):
         '''compute output shape'''
         assert isinstance(input_shape, dict)
         shape_en = input_shape['enocderHs']
-        output_shape = shape_en[0], 1, shape_en[2]
+        if self.return_aweights:
+            output_shape = [(shape_en[0], shape_en[2]), (shape_en[0], shape_en[1])]
+            return output_shape
+        output_shape = shape_en[0], shape_en[2]
         return output_shape
-
-    def compute_mask(self, inputs, mask=None):
-        '''compute mask'''
-        # pylint: disable=W0612,W0613
-        return None
-        
+              
