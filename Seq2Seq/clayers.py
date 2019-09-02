e11ce77ba8707e4869a66e6291103bb5dd8a8094
Uday Bhaskar <udaybhaskarpaila@gmail.com>
2019-09-02 21:31:08 +0530

    corrections in Bahdanau attention

--- Seq2Seq/clayers.py
+++ Seq2Seq/clayers.py
@@ -10,7 +10,7 @@ class BahdanauAttention(Layer):
     BahdanauAttention:
     Implemented based on below paper
         https://arxiv.org/pdf/1409.0473.pdf
-        attention_weights = probability_fn(Va * tanh(Wa*Ht+Ua*Hs+b))
+        attention_weights = probability_fn((Va * tanh(Wa*Ht+Ua*Hs+b))/sqrt(scaling_factor))
     # Arguments
         units = number of hidden units to use.
         probability_fn = probability function to get probabilities(weights for attention)
@@ -18,6 +18,7 @@ class BahdanauAttention(Layer):
                          function which takes input distribution and returns probability dist.
         dropout_rate = dropout for attention weights (between 0 and 1, 0 - no dropout).
         return_aweights = Bool, whether to return attention weights or not.
+        scaling_factor = int/float to scale the score vector. default None=1
         weights_initializer = initializer for weight matrix
         bias_initializer = initializer for bias values
         weights_constraint = Constraint function applied to the weights
@@ -34,12 +35,16 @@ class BahdanauAttention(Layer):
                             shape - (Batchsize, dec_hidden_size)
         mask = You can apply mask for padded values or any custom values
                while calculating attention.
+               if you are giving mask for encoder and deocoder then you have
+               to give a dict similar to inputs. (keys: enocderHs, decoderHt)
+               else you can give only for enocoder normally.(one tensor)
                mask shape should be (Batchsize, encoder_seq_len)
     '''
     def __init__(self, units,
                  probability_fn='softmax',
                  dropout_rate=0,
                  return_aweights=False,
+                 scaling_factor=None,
                  weights_initializer='he_normal',
                  bias_initializer='zeros',
                  weights_constraint=None,
@@ -51,9 +56,9 @@ class BahdanauAttention(Layer):
         super(BahdanauAttention, self).__init__(**kwargs)
 
         self.units = units
-        self.probability_fn = probability_fn
         self.dropout_rate = dropout_rate
         self.return_aweights = return_aweights
+        self.scaling_factor = scaling_factor
         self.probability_fn = cget(probability_fn)
         self.weights_initializer = weights_initializer
         self.bias_initializer = bias_initializer
@@ -70,6 +75,7 @@ class BahdanauAttention(Layer):
         self._va = layers.Dense(1, kernel_initializer=self.weights_initializer,\
             bias_initializer=self.bias_initializer, kernel_constraint=self.weights_constraint,\
                 bias_constraint=self.bias_constraint, name=self.name+"Va")
+        self.supports_masking = True
 
 
     def build(self, input_shape):
@@ -93,9 +99,17 @@ class BahdanauAttention(Layer):
         if ('enocderHs' not in inputs.keys())or ('decoderHt' not in inputs.keys()):
             raise ValueError("Input to the layer must be a dict with \
             keys=['enocderHs','decoderHt']")
+        if isinstance(mask, dict):
+            mask_enc = mask.get('enocderHs', None)
+            mask_dec = mask.get('decoderHt', None)
+        else:
+            mask_enc = mask
+            mask_dec = None
 
-        enc_out, dec_prev_hs = tf.cast(inputs['enocderHs'], tf.float64), \
-            tf.cast(inputs['decoderHt'], tf.float64)
+        enc_out, dec_prev_hs = tf.cast(inputs['enocderHs'], tf.float32), \
+            tf.cast(inputs['decoderHt'], tf.float32)
+        if mask_dec is not None:
+            dec_prev_hs = dec_prev_hs * tf.cast(mask_dec, dec_prev_hs.dtype)
 
         # decprev_hs - Decoder hidden shape == (batch_size, hidden size)
         # hidden_with_time_axis shape == (batch_size, 1, hidden size)
@@ -105,22 +119,26 @@ class BahdanauAttention(Layer):
         score = self._va(tf.nn.tanh(self._wa(dec_hidden_with_time_axis) + self._ua(enc_out)))
         score = tf.squeeze(score, [2])
 
-        if mask is not None:
-            score = score + (tf.cast(mask, score.dtype)*-1e9)
+        if self.scaling_factor is not None:
+            score = score/tf.sqrt(self.scaling_factor)
+
+        if mask_enc is not None:
+            score = score + (tf.cast(mask_enc, score.dtype)*-1e9)
 
         # attention_weights shape == (batch_size, max_length)
         attention_weights = self.probability_fn(score, axis=-1)
-        #(batch_size, max_length, 1)
-        attention_weights = tf.expand_dims(attention_weights, 2)
+        #(batch_size, 1, max_length)
+        attention_weights = tf.expand_dims(attention_weights, 1)
 
         if self.dropout_rate != 0:
             attention_weights = tf.nn.dropout(attention_weights, rate=self.dropout_rate)
 
         #context_vector shape (batch_size, hidden_size)
-        context_vector = tf.tensordot(attention_weights, enc_out, axes=2, name='context_vector')
+        context_vector = tf.matmul(attention_weights, enc_out)
+        context_vector = tf.squeeze(context_vector, 1, name="context_vector")
 
         if self.return_aweights:
-            return context_vector, tf.squeeze(attention_weights, 2, name='attention_weights')
+            return context_vector, tf.squeeze(attention_weights, 1, name='attention_weights')
         return context_vector
 
     def compute_output_shape(self, input_shape):
@@ -132,4 +150,37 @@ class BahdanauAttention(Layer):
             return output_shape
         output_shape = shape_en[0], shape_en[2]
         return output_shape
-              
+
+
+class LuongeAttention:
+    '''
+    LuongeAttention
+    Implemented based on below paper
+    https://arxiv.org/pdf/1508.04025.pdf
+    '''
+    def __init__(self, units,
+                 attention_type,
+                 probability_fn='softmax',
+                 dropout_rate=0,
+                 return_aweights=False,
+                 scaling_factor=None,
+                 weights_initializer='he_normal',
+                 bias_initializer='zeros',
+                 weights_constraint=None,
+                 bias_constraint=None, **kwargs):
+
+        if 'name' not in kwargs:
+            kwargs['name'] = ""
+        super(LuongeAttention, self).__init__(**kwargs)
+
+        self.units = units
+        self.attention_type = attention_type
+        self.probability_fn = cget(probability_fn)
+        self.dropout_rate = dropout_rate
+        self.return_aweights = return_aweights
+        self.scaling_factor = scaling_factor
+        self.weights_initializer = weights_initializer
+        self.bias_initializer = bias_initializer
+        self.weights_constraint = weights_constraint
+        self.bias_constraint = bias_constraint
+        
